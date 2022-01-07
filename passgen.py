1ac085f626865f838c9ac566210215fbdbf1ac8d
unknown <adjeidag@gmail.com>
2022-01-07 23:12:15 +0000

    pseudo code

---
+++ passgen.py
@@ -0,0 +1,20 @@
+import string
+import random
+
+upper_case = string.ascii_uppercase
+lower_case = string.ascii_lowercase  
+
+
+lower = list(lower_case)  
+upper = list(upper_case)
+special = ['!','1', '@','2', '#','3', '%','4','5','*','6', '(','7', ')','8', '-','9', '+','0']
+password = ""
+
+def generate():
+    for i in range(19):
+        num = random.randint(0, 19)
+        
+
+    
+
+
